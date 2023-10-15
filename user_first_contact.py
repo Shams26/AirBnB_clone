d514c4ab2e0437125f216ed13d79aaea878f95b7
Shams26 <123319812+Shams26@users.noreply.github.com>
2023-10-15 01:11:10 +0100

    my_beautiful_code

---
+++ user_first_contact.py
@@ -0,0 +1,32 @@
+def get_user_name():
+  """Prompts the user for their name and returns it.
+
+  Returns:
+    The user's name.
+  """
+  user_name = input("What is your name? ")
+  return user_name
+
+
+def greet(user_name):
+  """Greets the user by name.
+
+  Args:
+    user_name: The user's name.
+
+  Returns:
+    A greeting message.
+  """
+  greeting = f"Hello, {user_name}!"
+  return greeting
+
+
+def main():
+  """The main function of the program."""
+  user_name = get_user_name()
+  greeting = greet(user_name)
+  print(greeting)
+
+
+if __name__ == "__main__":
+  main()
